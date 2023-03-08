db7be04ef1b8a5b92891a4557ff059640a4e510c
Hernan Vivani <hernan@vivani.net>
2023-03-08 14:43:04 +0000

    fixing Status led

--- R2D2/src/main/java/net/vivani/robotics/StatusLed.java
+++ R2D2/src/main/java/net/vivani/robotics/StatusLed.java
@@ -10,19 +10,23 @@ public class StatusLed
     private static GpioController gpio;
     private static GpioPinDigitalOutput ledPin;
 
-    public StatusLed() {}
-
-    public void standBy() {
-
-        System.out.println("Starting Blinking Led");
-
-        try {
+    public StatusLed() {
+        try{
             /** create gpio controller */
             gpio = GpioFactory.getInstance();
             // Pi4J GPIO_29 is GPIO_21
             // Pi4J GPIO_16 is GPIO_27
             ledPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
+        } catch (Exception e) {
+              e.printStackTrace();
+        }
+    }
 
+    public void standBy() {
+
+        System.out.println("Starting Status Led");
+
+        try {
             int milliseconds=3000;
 
             /** Blink every second*/
@@ -30,37 +34,26 @@ public class StatusLed
 
             System.out.println("CTRL-C to stop");
 
-           // System.out.println("LED is blinking every 3 seconds. (CTRL-C) to abort...");
-           // while (true) {
-           //     //sleeping now for 500 milliseconds...
-           //     Thread.sleep(500);
-           // }
             } catch (Exception e) {
               e.printStackTrace();
             }
         }
 
     public void stop() {
-        System.out.println("Stopping Blinking Led");
+        System.out.println("Stopping Status Led");
         ledPin.low();
     }
 
     public void running() {
         try {
-            /** create gpio controller */
-            gpio = GpioFactory.getInstance();
-            // Pi4J GPIO_29 is GPIO_21
-            // Pi4J GPIO_16 is GPIO_27
-            ledPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
-
             int milliseconds=500;
 
             /** Blink every second*/
             ledPin.blink(milliseconds);
 
-            } catch (Exception e) {
+        } catch (Exception e) {
               e.printStackTrace();
-            }
+        }
 
     }
 
