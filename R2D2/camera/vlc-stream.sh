4c147d898ec5cda11b43d1dab5e13e804692f8ea
Hernan Vivani <hernan@vivani.net>
2023-03-08 16:24:34 +0000

    reconfiguring settings for vlc camera streaming

--- R2D2/camera/vlc-stream.sh
+++ R2D2/camera/vlc-stream.sh
@@ -3,6 +3,8 @@
 
 ##Opens Video Streaming on a preview window on a specific location.
 ## https://www.tomshardware.com/how-to/stream-live-video-raspberry-pi
-## ihttps://blog.robertelder.org/commands-raspberry-pi-camera/
+## https://blog.robertelder.org/commands-raspberry-pi-camera/
 
-raspivid -o - -t 0 -w 640 -h 480 -fps 24  | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8083/}' :demux=h264
+## on client computer open VLC --> Open Network Stream --> rtsp://YOUR-IP:8083/
+
+/usr/bin/raspivid -o - -t 0 -w 640 -h 480 -fps 24  | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8083/}' :demux=h264
